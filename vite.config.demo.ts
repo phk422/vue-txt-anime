661c02abaf2dabcd472e27c2279b58524607ac05
phk422 <1769476785@qq.com>
2024-02-22 15:11:28 +0800

    fix(config): base value

--- vite.config.demo.ts
+++ vite.config.demo.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'vue-txt-anime',
+  base: '/vue-txt-anime',
   plugins: [vue()],
   build: {
     outDir: 'demo',
